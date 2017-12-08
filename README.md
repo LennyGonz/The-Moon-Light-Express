192e9ce06c7106d07cb83624bd5c27b840109b40
lennygonz <lenny.gonzaleznyc@gmail.com>
2017-12-07 20:12:02 -0500

    restructed directory

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # The-Moon-Light-Express
 CSc 33600 - Database Project: RailRoad System
+The 
--- src/themoonlightexpress/themoonlightexpress/settings.py
+++ src/themoonlightexpress/themoonlightexpress/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'displaydata',
+    'homepage',
+    'reservation',
+    'themoonlightexpressmain',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
