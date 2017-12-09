7cdaef9175c89da1aa5d133d1c6eb6a7476e4d38
lennygonz <lenny.gonzaleznyc@gmail.com>
2017-12-09 15:17:39 -0500

    GUI 90% done

--- src/themoonlightexpress/themoonlightexpress/urls.py
+++ src/themoonlightexpress/themoonlightexpress/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('homepage.urls')),
+    #url(r'^search/', include('themoonlightexpressmain.urls')),  # including the urls belonging to amtrak app
+    #url(r'^reservations/', include('reservation.urls')),
     url(r'^admin/', admin.site.urls),
+    #url(r'^displaytables/', include('displaydata.urls')),
 ]
