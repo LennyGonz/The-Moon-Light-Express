7d161839d5054a49c4bf9bf558adf68c74947eba
Rohan Swaby <rohan_swaby51@yahoo.com>
2017-12-13 19:44:07 -0500

    simple reservation form

--- src/themoonlightexpress/reservation/forms.py
+++ src/themoonlightexpress/reservation/forms.py
@@ -7,16 +7,13 @@ class ReservationsForms(forms.Form):
     # here we will get the option  for the resercation
     station_station = forms.ModelChoiceField(queryset=Stations.objects.all())
     end_station = forms.ModelChoiceField(queryset=Stations.objects.all())
-    date = forms.DateField(help_text="enter date")
+    date = forms.DateField(help_text="month-day-year")
 
 
 class PassengerForms(forms.Form):
     # here we need to get all the information for the passenger
-    fname = forms.CharField(required=True,max_length=30,help_text="Required")
-    lname = forms.CharField(required=True,max_length=30,help_text="Required")
-    password = forms.CharField(required=True,max_length=30,help_text="Required")
-    preferred_card_number = forms.CharField(required=True,max_length=30,help_text="Required")
-    preferred_billing_address = forms.CharField(required=True,max_length=30,help_text="Required")
-
-    class Meta:
-        model = Passengers
+    fname = forms.CharField(required=True, max_length=30, help_text="Required")
+    lname = forms.CharField(required=True, max_length=30, help_text="Required")
+    preferred_card_number = forms.CharField(required=True, max_length=30, help_text="Required")
+    preferred_billing_address = forms.CharField(required=True, max_length=30, help_text="Required")
+    seat_time = forms.CharField(help_text="Child-Adult")
--- src/themoonlightexpress/reservation/views.py
+++ src/themoonlightexpress/reservation/views.py
@@ -2,17 +2,35 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import *
+from .carlos_functions_djago import *
 
 
 # Create your views here.
-def reservations(request, methods=["GET","POST"]):
-    if request.method == 'POST':
+# Create your views here.
+def TrainLookUpView(request, METHOD=["GET", "POST"]):
+    if request.METHOD == 'POST':
         form = ReservationsForms(request.POST)
-        print(form)
         if form.is_valid():
-            return HttpResponse("Reservation made")
+            start = request.POST['start_station']
+            end = request.POST['end_station']
+            train_direction = direction(start, end)
+            day = MF(int(request.POST['date_month']), int(request.POST['date_day']), int(request.POST['date_year']))
+            trainsid = trainsavible(train_direction, day)
+
+            # return render(request, "displaytrain.html", resource)
+            return HttpResponse(trainsid)
     else:
         form = ReservationsForms()
         return render(request, 'reservation.html', {'form': form})
+
+
+def PassengerView(request, METHOD=["GET", "POST"]):
+    if request.method == 'POST':
+        print(request.POST)
+        form = PassengerForms(request.POST)
+        if form.is_valid():
+            ## here we extract information to save the information
+            ## to register this in passenger
+            pass
 
 
